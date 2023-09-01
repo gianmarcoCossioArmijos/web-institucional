88a33efc04bfcb45d31903d3aa4e6c605b6ed85a
gianmarcoCossioArmijos <117977501+gianmarcoCossioArmijos@users.noreply.github.com>
2023-09-01 18:52:38 -0500

    logo universidad

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { showAsideMenu, hideAsideMenu } from '../../store/aside/asideSlice';
 
 import { AiOutlineMenuUnfold } from "react-icons/ai";
+import logo from '../../assets/university.png'
 
 const Header = ({ bg, auth }) => {
 
@@ -33,9 +34,14 @@ const Header = ({ bg, auth }) => {
   return (
     <header className={`w-full p-3 flex justify-between ${bg? bg : ""} gap-4 text-white`}>
 
-        <Link to="/" className="my-auto font-bold text-xl sm:text-2xl self-center">
-            <h1>
-                EduApp
+        <Link to="/" className="my-auto flex gap-2 text-xl sm:text-2xl self-center">
+
+            <img src={logo} alt="logo_universidad" className='w-[40px] h-[40px] self-center'/>
+
+            <span className='self-center'>|</span>
+
+            <h1 className='font-bold self-center'>
+              EduApp
             </h1>
         </Link>
 
